3857c48005fe082775f1386d323a90c71a746efc
Sani Kumar Sharma <82691077+sani9525@users.noreply.github.com>
2024-01-01 11:23:45 +0530

    Update 1_variable.js

--- Web Dev/JS-Part-1 with drop x out/1_variable.js
+++ Web Dev/JS-Part-1 with drop x out/1_variable.js
@@ -1,6 +1,6 @@
 var b=3; // not more used in recent
 let b=3; // mostly used in js, and this can be change further
-const c=7; // mostly used in js, and this is const variable once its declare it can not be can.
+const c=7; // mostly used in js, and this is const variable once its declare it can not be change.
 
 
 console.log(a,b,c);
