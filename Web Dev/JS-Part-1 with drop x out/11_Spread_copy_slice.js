0ec1ff1d4337f0ce0c48401972f883b2fcfa3fef
Sani Kumar Sharma <82691077+sani9525@users.noreply.github.com>
2024-02-16 17:07:12 +0530

    Update 11_Spread_copy_slice.js

--- Web Dev/JS-Part-1 with drop x out/11_Spread_copy_slice.js
+++ Web Dev/JS-Part-1 with drop x out/11_Spread_copy_slice.js
@@ -2,6 +2,6 @@ let arr1=["Sani",345,true];
 
 let arr2=arr1.slice(0); // this slice property is used to copy from one array to another array.
 
-arr1.push("burg"); // and this push function will insert value in arr2
+arr1.push("burg"); // and this push function will insert value in arr1
 console.log(arr2);
 console.log(arr1);
