d984015602c5d214d1f826f2f1edffe382c96920
Sani Kumar Sharma <82691077+sani9525@users.noreply.github.com>
2024-02-16 17:24:07 +0530

    Update 18_sets.js

--- Web Dev/JS-Part-1 with drop x out/18_sets.js
+++ Web Dev/JS-Part-1 with drop x out/18_sets.js
@@ -5,6 +5,7 @@ const s= new Set([10,200,30,40,50,30]);
 s.add(38)
 s.add("hello")
 console.log(s);
+console.log(s.size);
 
 let len=0;
 for(let ele of s){
@@ -14,4 +15,4 @@ console.log(len);
 
 // set is used to store the unique values in javascript and its use 
 // add() function to insert any value in set.
-// to know the set length we have to write a loop till end and count all the element that will be our size of set.
+// to know the set length we can use setName.size or we have to write a loop till end and count all the element that will be our size of set.
